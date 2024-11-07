b03d6fb73e782df8959acb581d5d132a6b775f30
Jpeter84 <jpeter84@vols.utk.edu>
2024-11-07 18:36:08 -0500

    added Cov and Coh
    Just missing implementation of excel sheet and way of solving for alpha

--- DriftFlux.py
+++ DriftFlux.py
@@ -56,7 +56,7 @@ Re= max([Reg,Ref])
 if Reg >= 0:
     Fr= (90-theta)/10
     C1=4*pcrit**2/(p*(pcrit-p))
-    L = (1-np.exp(-C1*alpha))/(1-np.exp(-C1)) #for steam-water
+ #for steam-water
     A1= 1/ [1+np.exp(-Re/60000)] # if Reg >Ref or Reg <0, otherwise Ref used instead
     B1= min ([.8, A1])
     K1=B1
@@ -75,20 +75,25 @@ if Reg >= 0:
         C3 = max([0.50, 2*np.exp(-np.abs(Ref) 1/60,000)])
     else:
         C3 = 2*np.exp(np.abs(Ref)/350000)**.4-1.75*np.abs(Ref)**.03*np.exp(-np.abs(Ref)/50000*(D1/Dh)**2)+(D1/Dh)**.25*np.abs(Ref)**.001
-    
     C7=(D2/Dh)**.6 
+    C8 = C7/(1-C7)
+
     if C7 >= 1:
         C4=1
     else:
         C4 = 1/(1-np.exp(-C8))
-    C8 = C7/(1-C7)
 
 
 
 
 
+    L = (1-np.exp(-C1*alpha))/(1-np.exp(-C1))
     Vgj=1.41*((pf-pg)*sigma*g*gc/pf**2)**(1/4)*(1-alpha)**K1*C2*C3*C4
+    K0= B1+(1-B1)*(pg/pf)**(1/4)
     Cov= L/(K0 + (1-K0)*alpha**r)
+    Coh=((1+alpha**.05)*(1-alpha)**2)Cov
+    Co=Fr*Cov+(1-Fr)*Coh
+
 
 
 
@@ -102,7 +107,5 @@ else:
 
 
 
-Vgj= (jg*alpha*Co*(jg+jf)/alpha)
-alpha=
-
-#try pusdssdsd
+# Vgj= (jg*alpha*Co*(jg+jf)/alpha)
+# alpha=
